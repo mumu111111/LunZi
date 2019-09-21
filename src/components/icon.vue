f8958d534bd1a3a1ff515fcd66f952c50e5e23b3
mumu111111 <2646709035@qq.com>
2019-09-21 23:41:45 +0800

    'icon'

---
+++ src/components/icon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg class="g-icon" @click="$emit('click', $events)">
+    <use :xlink:href="`#i-${name}`">
+  </svg>
+</template>
+
+<script>
+import "./svg";
+export default {
+  name: "Icon",
+  props: ["name"]
+};
+</script>
+
+<style scoped lang="scss">
+.g-icon {
+  width: 1em;
+  height: 1em;
+}
+</style>

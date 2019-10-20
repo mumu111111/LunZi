963eed91611940082a7c96905baa63261058a0ba
mumu111111 <2646709035@qq.com>
2019-10-20 18:44:07 +0800

    '一个svg组件'

---
+++ src/components/Icon/icon-svg-2/icon2.vue
@@ -0,0 +1,45 @@
+<template>
+    <svg class="svg-icon" aria-hidden="true">
+        <use :xlink:href="iconName"></use>
+    </svg>
+
+</template>
+
+<script>
+export default {
+  name: "icon",
+  props: {
+    iconClass: {
+      type: String,
+      require: true
+    }
+  },
+  computed: {
+    iconName() {
+      return `#icon-${this.iconClass}`;
+    }
+  }
+};
+</script>
+
+<style>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
+
+-------------------------------
+//组件使用步骤
+
+//1.引入svg组件
+// import Icon from '@/components/Icon'
+
+//2.全局注册icon组件
+// Vue.component('icon', Icon)
+
+//3.直接可以使用这个组件了  (注意大小写 iconClass)
+// <icon icon-class="pass"/>

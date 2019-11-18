c743f375e2fc609b35499fb067021a3dd9b37e82
mumu111111 <2646709035@qq.com>
2019-11-19 06:26:29 +0800

    'update'

--- src/components/grid/col.vue
+++ src/components/grid/col.vue
@@ -23,6 +23,7 @@ export default {
   props: {
     span: [Number, String],
     offset: [Number, String],
+    //自定义不同尺寸适配 响应式  js就生成不同的类名   不同类名对应设置好的css样式  
     ipad: {},
     narrowPc: {},
     pc: {},
@@ -38,6 +39,18 @@ export default {
   },
   computed: {
     colClass() {
+      //判断 存不存在
+     if(this.span){
+       return {}
+     }
+     if(this.offset){
+       return {}
+     }
+     //数据类型 数组 方法多 push 好操作
+     //return {} 最后必须合二为一 所以在一开始先写一个[]  有什么操作结果都放在里面 一个里面
+
+     //这么多操作且重复执行 可以放在一个函数里 好调用
+
       let { offset, span } = this;
       return {`col-${span}`, offset-${offset}`}      
       // return {} / [];
@@ -52,5 +65,6 @@ export default {
 </script>
 
 <style scoped>
-
+@media (min-width: ) {
+}
 </style>

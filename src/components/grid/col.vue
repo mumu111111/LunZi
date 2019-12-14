7ee89585f0c57359171c99b16a9b0632983f4643
mumu111111 <2646709035@qq.com>
2019-12-14 16:30:46 +0800

    'col-update'

--- src/components/grid/col.vue
+++ src/components/grid/col.vue
@@ -1,5 +1,3 @@
-<!-- 列 -->
-
 <template>
   <div class="col" :class="colClass" :style="colStyle">
     <slot></slot>
@@ -7,64 +5,53 @@
 </template>
 
 <script>
-let validator = value => {
-  let keys = Object.keys(value);
-  let valid = true;
-  keys.forEach(key => {
-    if (!["span", "offset"].includes(key)) {
-      valid = false;
-    }
-  });
-  return valid;
-};
-
 export default {
-  name: "Col",
-  props: {
-    span: [Number, String],
-    offset: [Number, String],
-    //自定义不同尺寸适配 响应式  js就生成不同的类名   不同类名对应设置好的css样式  
-    ipad: {},
-    narrowPc: {},
-    pc: {},
-    widePc: {}
-  },
+  name: "",
   data() {
-    return {
-      gutter: 0
-    };
+    return;
   },
-  methods: {
-    
+  props: {
+    span: {
+      type: [Number, String],
+      validator(value) {
+        return ["offset"].indexOf(value) >= 0;
+      }
+    },
+    offset: {
+      type: [Number, String],
+      validator(value) {
+        return ["offset"].indexOf(value) >= 0;
+      }
+    }
   },
   computed: {
-    colClass() {
-      //判断 存不存在
-     if(this.span){
-       return {}
-     }
-     if(this.offset){
-       return {}
-     }
-     //数据类型 数组 方法多 push 好操作
-     //return {} 最后必须合二为一 所以在一开始先写一个[]  有什么操作结果都放在里面 一个里面
-
-     //这么多操作且重复执行 可以放在一个函数里 好调用
-
-      let { offset, span } = this;
-      return {`col-${span}`, offset-${offset}`}      
-      // return {} / [];
+    rowStyle() {
+      return {};
     },
-    colStyle() {
-      let { offset, span } = this;
+    rowClass() {
+      if (span) {
+        return [`span-${span}`];
+      }
+      if (offset) {
+        return [`offset-${offset}`];
+      }
 
-      // return {} / [];
+      return [];
     }
   }
 };
 </script>
-
-<style scoped>
-@media (min-width: ) {
+<style scoped lang="scss">
+.col {
+  @for $n from 1 through 24 {
+    &.span-#{$n} {
+      width: $n/24*100%;
+    }
+    &.offset-#{$n} {
+      margin-left: $n/24*100%;
+    }
+  }
 }
 </style>
+
+

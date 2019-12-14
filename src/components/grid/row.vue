82571cc9e712a9312c7a2bf4435b3bd6a6b3b071
mumu111111 <2646709035@qq.com>
2019-12-14 11:03:45 +0800

    'row'

--- src/components/grid/row.vue
+++ src/components/grid/row.vue
@@ -9,21 +9,22 @@
 export default {
   name: "Row",
   props: {
+    // 传入的参数 可以控制元素间隔
     gutter: {
       type: [Number, String]
     },
     align: {
+      //left right center
       type: String,
       validator(value) {
-        //自定义验证  value必须匹配下面字符串的一个
-        //自己想办法去加入一些能用到的属性值
+        //检测是否存在 true false 有时候不一定有
         return ["left", "right", "center"].indexOf(value) >= 0;
       }
     }
   },
   computed: {
     rowStyle() {
-      let { gutter } = this;
+      let { gutter } = this; //取gutter  gutter = this.gutter
       return {
         marginLeft: -gutter / 2 + "px",
         marginRight: -gutter / 2 + "px"
@@ -33,19 +34,13 @@ export default {
       let { align } = this;
       return [align && `align-${align}`];
     }
-  },
-  mounted() {
-    this.$children.forEach(vm => {
-      vm.gutter = this.gutter;
-    });
   }
 };
 </script>
-<style scoped lang= "scss">
+<style scoped lang="scss">
 .row {
   display: flex;
   flex-wrap: wrap;
-
   &.align-left {
     justify-content: flex-start;
   }
@@ -57,6 +52,11 @@ export default {
   }
 }
 </style>
+
+
+
+
+
 
 
 
